f464bfdafebbc6f09936fd80166cf6459f7fd6c6
luiferDev <luifer991@gmail.com>
2025-11-22 23:42:12 -0500

    fix: image gallery issues for being too large

--- src/features/projects/components/ProjectGallery.tsx
+++ src/features/projects/components/ProjectGallery.tsx
@@ -21,36 +21,38 @@ interface ProjectGalleryProps {
 
 export default function ProjectGallery({ images }: ProjectGalleryProps) {
   return (
-    <Carousel
-      className="w-full md:ml-12"
-      opts={{
-        align: 'start',
-        loop: true,
-      }}
-    >
-      <CarouselContent className="-ml-1 not-prose">
-        {images.map((image, index) => (
-          <CarouselItem key={index} className="pl-1 relative">
-            <div>
-              <Card>
-                <CardContent>
-                  <AspectRatio ratio={16 / 9} className="h-full w-full">
-                    <img
-                      src={image.src}
-                      alt={image.alt}
-                      className="h-full w-full rounded-md object-cover"
-                      width={600}
-                      height={600}
-                    />
-                  </AspectRatio>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className=" md:left-[-3rem] absolute  left-4 top-1/2 -translate-y-1/2" />
-      <CarouselNext className=" md:right-[-3rem] absolute right-4  top-1/2 -translate-y-1/2" />
-    </Carousel>
+    <div className="relative max-w-3xl mx-auto">
+      <Carousel
+        className="w-full md:ml-12"
+        opts={{
+          align: 'start',
+          loop: true,
+        }}
+      >
+        <CarouselContent className="-ml-1 not-prose">
+          {images.map((image, index) => (
+            <CarouselItem key={index} className="pl-1 relative">
+              <div>
+                <Card>
+                  <CardContent className="p-2">
+                    <AspectRatio ratio={16 / 9} className="h-full w-full">
+                      <img
+                        src={image.src}
+                        alt={image.alt}
+                        className="h-full w-full rounded-md object-cover"
+                        width={400}
+                        height={225}
+                      />
+                    </AspectRatio>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-2 top-1/2 -translate-y-1/2" />
+        <CarouselNext className="right-2 top-1/2 -translate-y-1/2" />
+      </Carousel>
+    </div>
   );
 }
