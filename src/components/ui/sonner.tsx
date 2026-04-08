67a6ed671e8d2c2b83e17d5fe8b67edd91f96d8c
luiferDev <luifer991@gmail.com>
2026-04-07 23:10:15 -0500

    modify featured, homescreen and sonner

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -23,3 +23,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 };
 
 export { Toaster };
+
+// Note: The sonner library adds tabindex="-1" to a section element for accessibility.
+// This triggers an a11y warning but is intentional for keyboard navigation.
+// See: https://github.com/emilkowalski/sonner/issues/285
